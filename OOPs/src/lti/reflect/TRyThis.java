8ee2d51fee942d7d37d23772161714cad0589129
AE206_PC3 <AE206_PC3@DESKTOP-RDBA9E3>
2018-08-08 17:58:36 +0530

    OOPs

---
+++ OOPs/src/lti/reflect/TRyThis.java
@@ -0,0 +1,18 @@
+package lti.reflect;
+
+public class TRyThis {
+
+	public static void main(String[] args) {
+try {
+	System.exit(0);
+}
+catch(Exception e) {
+	e.printStackTrace();
+	
+}
+finally {
+	System.out.println("Finally executed");
+}
+	}
+
+}
